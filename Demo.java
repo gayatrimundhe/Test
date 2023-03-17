a0c31f2642a4387dc93d9e3227299151f3b1d627
gayatrimundhe <128040524+gayatrimundhe@users.noreply.github.com>
2023-03-17 11:36:08 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class Demo {
+	
+		public static void main(String args[]) {
+		int x = 0;
+		if (x==0) {
+		 System.out.println("hello");
+		}
+		else {
+		 System.out.println("hi");
+		}
+		
+		}
+	
+
+}
